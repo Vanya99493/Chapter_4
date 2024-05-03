19af84b61802e95091441800b5ce9f427d8c4e48
Ваня Задорожній <vanyazadorozhnii99493@gmail.com>
2024-05-03 13:49:01 +0300

    Complete 1-st task

---
+++ Assets/1_UIComponents/Scripts/FigureModule/FigureView.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace _1_UIComponents.FigureModule
+{
+    public class FigureView : MonoBehaviour
+    {
+        [SerializeField] private float _transitionMultiplier;
+        [SerializeField] private float _scaleMultiplier;
+        
+        public void ChangeXPosition(float xPosition)
+        {
+            transform.position = new Vector3(xPosition * _transitionMultiplier, transform.position.y, transform.position.z);
+        }
+        
+        public void ChangeYPosition(float yPosition)
+        {
+            transform.position = new Vector3(transform.position.x, yPosition * _transitionMultiplier, transform.position.z);
+        }
+
+        public void ChangeXScale(float xScale)
+        {
+            transform.localScale = new Vector3(xScale * _scaleMultiplier, transform.localScale.y, transform.localScale.z);
+        }
+
+        public void ChangeYScale(float yScale)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, yScale * _scaleMultiplier, transform.localScale.z);
+        }
+    }
+}
---
+++ Assets/1_UIComponents/Scripts/Game.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace _1_UIComponents
+{
+    public class Game : MonoBehaviour
+    {
+        public void DebugMessage(string message)
+        {
+            Debug.Log(message);
+        }
+    }
+}
